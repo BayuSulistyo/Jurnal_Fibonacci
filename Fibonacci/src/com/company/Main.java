df82a8e1b5eed4b7f0e0929dfcbf630beb83c800
Bayu sulistyo <37774063+Bayu-6706164070@users.noreply.github.com>
2018-03-28 10:51:25 +0700

    Add files via upload

---
+++ Fibonacci/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner show = new Scanner(System.in);
+        System.out.print("===Deret Fibonacci===\n");
+        System.out.print("Input : ");
+        int a = show.nextInt();
+        fibonacci(a);
+    }
+    public static void fibonacci(int a){
+        long x[] = new long[a];
+        x[0] = 1;
+        x[1] = 1;
+        for(int i = 2; i < a; i++) {
+            x[i] = x[i-1] + x[i-2];
+        }
+        for (int i = 0; i < a; i++) {
+            System.out.print(x[i] +  " ");
+        }
+    }
+}
